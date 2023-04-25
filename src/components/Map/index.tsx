d91552fbc92b248f4ca25bec1e51d379fa765b1a
yterasaka <yuki.terasaka@gmail.com>
2023-04-25 23:00:04 +0200

    Change the address on the map

--- src/components/Map/index.tsx
+++ src/components/Map/index.tsx
@@ -19,7 +19,7 @@ const Map = () => {
       <div className={styles.googleMapWrapper}>
         <iframe
           className={styles.googleMapIframe}
-          src="https://www.google.com/maps/embed?pb=!1m26!1m12!1m3!1d2663.3599736180336!2d7.4726105771375835!3d48.122581002204164!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!4m11!3e2!4m3!3m2!1d48.120407799999995!2d7.4764032!4m5!1s0x4791699b7583f77d%3A0x2d191e5eba3cfb1f!2sRue%20de%20Riedwihr%2C%2068320%20Jebsheim%2C%20Frankreich!3m2!1d48.122648999999996!2d7.472792999999999!5e0!3m2!1sde!2sde!4v1682405089470!5m2!1sde!2sde"
+          src="https://www.google.com/maps/embed?pb=!1m26!1m12!1m3!1d1331.721450023588!2d7.474399639089827!3d48.12098159281043!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!4m11!3e2!4m3!3m2!1d48.120416299999995!2d7.476376999999999!4m5!1s0x4791699b42b9eeb3%3A0x8806cc7a62bc3fb6!2s15%20Rue%20de%20Riedwihr%2C%2068320%20Jebsheim%2C%20Frankreich!3m2!1d48.1216983!2d7.475259899999999!5e0!3m2!1sde!2sde!4v1682456267242!5m2!1sde!2sde"
           style={{ border: 0 }}
           allowFullScreen={true}
           loading="lazy"
