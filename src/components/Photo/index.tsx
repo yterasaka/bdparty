5566883a143f6775421be0f14e055daa3c309742
yterasaka <yuki.terasaka@gmail.com>
2023-05-21 07:56:40 +0200

    Update: add photo link

--- src/components/Photo/index.tsx
+++ src/components/Photo/index.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import styles from "./index.module.css";
 import camera from "../../../public/Images/camera.jpg";
+import merci from "../../../public/Images/merci.jpg";
 
 const Photo = () => {
   return (
@@ -10,7 +11,24 @@ const Photo = () => {
       </div>
       <div className={styles.textContainer}>
         <h4 className={styles.title}>Cheeeeeeeese</h4>
-        <p className={styles.text}>Soon, galerie photo de la jebtzparty.</p>
+        <Image
+          src={merci}
+          width={450}
+          height={600}
+          alt="Merci"
+          className={styles.image}
+        />
+        <p className={styles.text}>
+          Merci à toutes et à tous pour cette journée, voici le{" "}
+          <a
+            href="https://photos.app.goo.gl/x5TmYJntEN9YexTu5"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            lien
+          </a>{" "}
+          des photos !
+        </p>
       </div>
     </div>
   );
